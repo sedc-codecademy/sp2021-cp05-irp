f1dd0de4ae965c6ff285e2bd0d06b75fb73cb9f1
Elena-r-code <75211054+Elena-r-code@users.noreply.github.com>
2021-09-30 19:45:31 +0200

    Delete Project directory

--- Project/Scripts/qr.js
+++
@@ -1,8 +0,0 @@
-// QR CODE
-var qrdata = document.getElementById('qr-data');
-var qrcode = new QRCode(document.getElementById('qrcode'))
-
-function generateQR() {
-    var data = qrdata.value;
-    qrcode.makeCode(data);
-}
